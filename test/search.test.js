3daebe9cd64a9696ba45d23cfb01aa5171bcdb02
Salvatore Mazzarino <salvatore.mazzarino@strv.com>
2016-06-04 18:13:10 +0200

    Use AVA as test runner

--- test/search.test.js
+++ test/search.test.js
@@ -1,46 +1,33 @@
-// TODO: find another solution instead to include it
-// at the beginning of the file
-import 'babel-polyfill'
-
-import Code from 'code'
-import Lab from 'lab'
+import test from 'ava'
 import Proxyquire from 'proxyquire'
 import Promise from 'bluebird'
 
 import ratesData from './rates-data'
 
-const lab = exports.lab = Lab.script()
-
-const suite = lab.suite
-const test = lab.test
-const expect = Code.expect
-
 const Seneca = Proxyquire('seneca', {})
-// FIXME: understand how to mock request
 const Search = Proxyquire('..', {
-  getAsync: (url, opts, res, payload) => {
-    return Promise.resolve(payload)
+  'request-promise': () => {
+    return Promise.resolve(ratesData)
   }
 })
 
-function createInstance (done) {
+function createInstance () {
   const opts = {}
 
   return Seneca(opts)
     .use(Search)
 }
 
-suite('role:search', () => {
-  suite('cmd:latest', () => {
-    test('returns the latest foreign exchange reference rates', (done) => {
-      const seneca = createInstance(done)
+test.beforeEach(t => {
+  const seneca = createInstance()
+  t.context.act = Promise.promisify(seneca.act, { context: seneca })
+})
 
-      seneca.act({ role: 'search', cmd: 'latest' }, (err, reply) => {
-        expect(err).to.not.exist()
-        expect(reply).to.exist()
-        expect(reply).to.be.an.object()
-        done()
-      })
-    })
-  })
+test('role:search,cmd:latest - returns the latest foreign exchange reference rates', async t => {
+  const act = t.context.act
+
+  const reply = await act({ role: 'search', cmd: 'latest' })
+
+  t.truthy(reply)
+  t.true(reply.fake)
 })
