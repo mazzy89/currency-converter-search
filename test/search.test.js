8e91efa9f8db48f3d7d3bd61939c700267f18611
Salvatore Mazzarino <salvatore.mazzarino@strv.com>
2016-06-06 21:51:16 +0200

    Added query param and test

--- test/search.test.js
+++ test/search.test.js
@@ -2,18 +2,27 @@ import test from 'ava'
 import Config from 'getconfig'
 import Proxyquire from 'proxyquire'
 import Promise from 'bluebird'
+import Url from 'url'
 
-import ratesData from './rates-data'
+import ratesDataEur from './rates-data-eur'
+import ratesDataUsd from './rates-data-usd'
 
 const Seneca = Proxyquire('seneca', {})
 const Search = Proxyquire('..', {
-  'request-promise': () => Promise.resolve(ratesData)
+  'request-promise': (opts) => {
+    const parsedUri = Url.parse(opts.uri, true)
+    if (parsedUri.query.base === 'USD') {
+      return Promise.resolve(ratesDataUsd)
+    }
+    else {
+      return Promise.resolve(ratesDataEur)
+    }
+  }
 })
 
 function createInstance () {
-  const opts = {}
-
   const fixer = Config.fixer
+  const opts = {}
 
   return Seneca(opts)
     .use(Search, fixer)
@@ -24,11 +33,23 @@ test.beforeEach(t => {
   t.context.act = Promise.promisify(seneca.act, { context: seneca })
 })
 
-test('role:search,cmd:latest - returns the latest foreign exchange reference rates', async t => {
+test('role:search,cmd:latest - returns the latest foreign exchange reference rates for default base (EUR)', async t => {
   const act = t.context.act
 
   const reply = await act({ role: 'search', cmd: 'latest' })
 
   t.truthy(reply)
-  t.true(reply.fake)
+  t.is(reply.base, 'EUR')
+  t.is(reply.date, '2016-06-02')
+})
+
+test('role:search,cmd:latest,query:USD - returns the latest foreign exchange reference rates for USD currency', async t => {
+  const act = t.context.act
+
+  // in this case the latest action will be matched
+  const reply = await act({ role: 'search', cmd: 'latest', query: 'USD' })
+
+  t.truthy(reply)
+  t.is(reply.base, 'USD')
+  t.is(reply.date, '2016-06-03')
 })
